288b1efbb205f1ddcb53976d919def9871669705
Enigma1994 <scc2111@naver.com>
2021-04-22 02:47:18 +0900

    게시물 작성날짜 저장

--- src/main/kotlin/test_1.kt
+++ src/main/kotlin/test_1.kt
@@ -1,3 +1,7 @@
+
+import java.text.SimpleDateFormat
+
+
 fun readLineTrim() = readLine()!!.trim()
 
 fun main() {
@@ -5,7 +9,7 @@ fun main() {
 
     var articlesLastId = 0   // 가장 마지막에 입력된 게시물 번호
 
-    val articles = mutableListOf<Article>()   // 리스트
+    val articles = mutableListOf<Article>()   // 리스트 , 변화 가능
 
 
     loop@ while (true) {
@@ -20,11 +24,14 @@ fun main() {
             }
             "article write" -> {
                 val id = articlesLastId + 1
+                val regDate = Util.getNowDateStr()
+                val updateDate = Util.getNowDateStr()
+
                 print("제목: ")
                 val title = readLineTrim()
                 print("내용: ")
                 val body = readLineTrim()
-                val article = Article(id, title, body)
+                val article = Article(id, regDate, updateDate, title, body)
 
                 println("${id}번 게시물이 작성되었습니다.")
 
@@ -33,11 +40,11 @@ fun main() {
                 articlesLastId = id
             }
             "article list" -> {
-                println("번호 / 제목")
+                println("번호 / 작성날짜 / 제목")
 
-                for ( article in articles) {
+                for (article in articles) {
 
-                    println("${article.id} / ${article.title}")
+                    println("${article.id} / ${article.regDate} / ${article.title}")
 
                 }
             }
@@ -48,7 +55,20 @@ fun main() {
     }
     println("== 게시판 관리 프로그램 끝 ==")
 }
-data class Article(val id:Int,
-                   val title:String,
-                   val body:String,) {
+
+data class Article(
+    val id: Int,
+    val regDate: String,
+    val updateDate: String,
+    val title: String,
+    val body: String,
+)
+
+object Util {
+    fun getNowDateStr(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+        return dateFormat.format(System.currentTimeMillis())
+
+    }
 }
